c47b9f1d2e91ea25297be421879fbdf68b8bdeab
lines14 <tonybass1414@yahoo.com>
2025-07-05 22:40:20 +0500

    add check bank account existing to purchase create method

--- controllers/purchase_controller.py
+++ controllers/purchase_controller.py
@@ -1,11 +1,15 @@
 from DTO import PurchaseDTO
-from models import Purchase
 from fastapi import Response
 from utils.data_utils import DataUtils
+from models import Purchase, BankAccount
 from utils.response_utils import ResponseUtils
 
 class PurchaseController:
     async def create_purchase(self, purchase: PurchaseDTO) -> Response:
         new_purchase = Purchase(**vars(purchase))
-        await new_purchase.create()
-        return await ResponseUtils.success(DataUtils.responses.purchase_created_message)
+        existing_bank_account = await BankAccount(id=new_purchase.account_id).get()
+        if existing_bank_account:
+            await new_purchase.create()
+            return await ResponseUtils.success(DataUtils.responses.purchase_created_message)
+        else:
+            return await ResponseUtils.error(*DataUtils.responses.bank_account_not_found_error)
