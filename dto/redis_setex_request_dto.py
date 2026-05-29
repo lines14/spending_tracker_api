3515aa8b6ece4b0cc2738aa73da06d039e69c5bf
lines14 <tonybass1414@yahoo.com>
2026-05-29 12:00:56 +0500

    fix DTOs

--- dto/redis_setex_request_dto.py
+++ dto/redis_setex_request_dto.py
@@ -1,6 +1,15 @@
 from dto.base import BaseDTO
+from pydantic import field_validator
 
 class RedisSetexRequestDTO(BaseDTO):
     name: str
-    time: str
+    time: int
     value: str
+
+    @field_validator('time', mode='before')
+    @classmethod
+    def cast_str_env_to_int(cls, value):
+        if isinstance(value, str):
+            return int(value)
+        
+        return value
--- dto/redis_setex_with_tags_request_dto.py
+++ dto/redis_setex_with_tags_request_dto.py
@@ -1,15 +1,5 @@
-from typing import List, Optional
-from pydantic import field_validator
+from typing import List
 from dto import RedisSetexRequestDTO
 
 class RedisSetexWithTagsRequestDTO(RedisSetexRequestDTO):
     tags: List[str]
-    time: Optional[int]
-
-    @field_validator('time', mode='before')
-    @classmethod
-    def cast_str_env_to_int(cls, value):
-        if isinstance(value, str):
-            return int(value)
-        
-        return value
